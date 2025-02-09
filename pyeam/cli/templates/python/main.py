280ac0cc40a1e8c4a7c5eeec0c0781c8c2e3af2e
beqm <brenomartinsmb@gmail.com>
2025-02-09 18:05:23 -0300

    fix: template with old feats

--- pyeam/cli/templates/python/main.py
+++ pyeam/cli/templates/python/main.py
@@ -3,10 +3,10 @@ from pyeam.tools import log
 
 @pyeam.command
 def say_hi(name: str):
-    return f"Hello from pyeam {name}!"
+    return f"Hello from pyeam, {name}!"
 
 if __name__ == "__main__":
-    log.Env.root(log.INFO)
+    log.Env.level(log.INFO)
     log.info("Hello from pyeam!")
 
     pyeam.Builder.default().invokers([say_hi]).run()
