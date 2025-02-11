0737a64260f7216d6084298ad43705a022925d01
beqm <brenomartinsmb@gmail.com>
2025-02-11 00:10:57 -0300

    update template main.py

--- pyeam/cli/templates/python/main.py
+++ pyeam/cli/templates/python/main.py
@@ -2,11 +2,11 @@ import pyeam
 from pyeam.tools import log
 
 @pyeam.command
-def say_hi(name: str):
-    return f"Hello from pyeam, {name}!"
+def example(name: str):
+    return f"Greetings from Pyeam {name}!"
 
 if __name__ == "__main__":
     log.Env.level(log.INFO)
     log.info("Hello from pyeam!")
 
-    pyeam.Builder.default().invokers([say_hi]).run()
+    pyeam.Builder.default().invokers([example]).run()
